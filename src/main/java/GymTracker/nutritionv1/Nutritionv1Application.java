3a3332b4f9ec15b243993fef000c081156a49ff1
José Vitor <jose.vitorw018@gmail.com>
2026-03-02 22:19:45 -0300

    Primeiro commit

---
+++ src/main/java/GymTracker/nutritionv1/Nutritionv1Application.java
@@ -0,0 +1,13 @@
+package GymTracker.nutritionv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nutritionv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nutritionv1Application.class, args);
+	}
+
+}
---
+++ src/test/java/GymTracker/nutritionv1/Nutritionv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package GymTracker.nutritionv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nutritionv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
