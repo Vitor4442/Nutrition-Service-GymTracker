a54f377239b8b14fef8636ee59f326fa88acaea4
José Vitor <jose.vitorw018@gmail.com>
2026-04-21 20:17:16 -0300

    Protegendo as chaves

--- src/main/java/GymTracker/nutritionv1/Security/JwtUtil.java
+++ src/main/java/GymTracker/nutritionv1/Security/JwtUtil.java
@@ -2,6 +2,7 @@ package GymTracker.nutritionv1.Security;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -11,8 +12,12 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
-    private static final String SECRET = "minha-chave-secreta-com-pelo-menos-32-bytes";
-    private static final long EXPIRATION = 1000 * 60 * 60;
+    @Value("${jwt.secret}")
+    private  String SECRET;
+
+    @Value("${jwt.expiration}")
+    private  long EXPIRATION;
+
     private final SecretKey key = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
 
     public String generateToken(String username) {
